01ead988ea244660e4e6c3954a0f5c95a9ae0100
cdm <chris.moores@gmail.com>
2020-09-30 16:42:39 +0100

    Added examples for fees and streaming events

---
+++ fees/get-fees-estimate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Script language: bash
+#
+# Talks to:
+# - Vega node (REST)
+#
+# Apps/Libraries:
+# - REST: curl
+
+# Note: this file uses smart-tags in comments to section parts of the code to
+# show them as snippets in our documentation. They are not necessary to be
+# included when creating your own custom code.
+#
+# Example of smart-tags:
+#  __something:
+# some code here
+# :something__
+
+source helpers.sh
+
+check_url "NODE_URL_REST" || exit 1
+
+# Coming soon.
