4cc77071e6bd22eaf62024ef16e9d67c060a2b85
cdm <chris.moores@gmail.com>
2020-10-02 17:39:21 +0100

    Bump requirement version for python grpc client, add event type for example

--- stream-events/stream-events-with-Vega-API-client.py
+++ stream-events/stream-events-with-Vega-API-client.py
@@ -43,15 +43,23 @@ markets = data_client.Markets(Empty()).markets
 market_id = markets[0].id
 # :get_market__
 
+print("Found market:")
+print(market_id)
+
+print("Connecting to stream...")
+
 # __stream_events:
 # Subscribe to the events bus stream for the marketID specified
 # Required: type field - default ALL
 # Optional: Market identifier - filter by market
 #           Party identifier - filter by party
 # By default, all events on all markets for all parties will be returned on the stream.
-subscribe_events_request = vac.api.trading.ObserveEventsRequest(marketID=market_id)
+all_types = vac.events.BUS_EVENT_TYPE_ALL
+subscribe_events_request = vac.api.trading.ObserveEventsRequest(type=[all_types], marketID=market_id)
 for stream_resp in data_client.ObserveEventBus(subscribe_events_request):
     for events in stream_resp.events:
         # All events (as per request filter) arriving over the channel/stream will be printed
         print(events)
 # :stream_events__
+
+print("Stream disconnected.")
