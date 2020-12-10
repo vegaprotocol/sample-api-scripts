537680c43f4aa89b6ece9a55f23c48186e6c911c
cdm <chris.moores@gmail.com>
2020-12-10 17:20:21 +0000

    Add REST proposal listings

--- propose-votes/manage-proposals.py
+++ propose-votes/manage-proposals.py
@@ -79,6 +79,39 @@ assert pubkey != ""
 print("Selected pubkey for signing")
 
 #####################################################################################
+#                            L I S T   P R O P O S A L S                            #
+#####################################################################################
+
+# There are two types of REST request for proposals on Vega:
+# 1 - MARKET proposals (/governance/market/proposals)
+# 2 - ASSET proposals (/governance/asset/proposals)
+# Note: In the future users will be able to call an endpoint to retrieve ALL proposals.
+
+# __get_proposals:
+# Request a list of proposals on a Vega network
+response = requests.get(node_url_rest + "/governance/market/proposals")
+helpers.check_response(response)
+proposals = response.json()
+print("Proposals:\n{}".format(json.dumps(proposals, indent=2, sort_keys=True)))
+# :get_proposals__
+
+proposalID = proposals["data"][0]["proposal"]["ID"]
+assert proposalID != ""
+print(f"Proposal found: {proposalID}")
+
+#####################################################################################
+#                         P R O P O S A L   D E T A I L S                           #
+#####################################################################################
+
+# __get_proposal_detail:
+# Request results of a specific proposal on a Vega network
+response = requests.get(node_url_rest + "/governance/proposal/" + proposalID)
+helpers.check_response(response)
+response_json = response.json()
+print("Proposal:\n{}".format(json.dumps(response_json, indent=2, sort_keys=True)))
+# :get_proposal_detail__
+
+#####################################################################################
 #                          P A R T Y   P R O P O S A L S                            #
 #####################################################################################
 
