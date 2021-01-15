b769afcb2700821f01e9ed47a297c1dce3babccb
cdm <chris.moores@gmail.com>
2021-01-15 18:28:32 +0000

    Improve graphql event bus streaming sample with ... on [type] syntax and examples

--- stream-events/stream-events.sh
+++ stream-events/stream-events.sh
@@ -27,14 +27,110 @@ check_url "NODE_URL_GRAPHQL" || exit 1
 url="$NODE_URL_REST/markets"
 response="$(curl -s "$url")"
 marketID="$(echo "$response" | jq -r '.markets[0].id')"
+echo $marketID
 # :get_market__
 
 # __stream_events:
-# Subscribe to the events bus stream for the marketID specified
-# Required: type field - NOTE: All event is not supported by GraphQL API.
+# Subscribe to the events bus stream for the marketID specified, 
+# Required: type field - Switch the event type with '... on [TYPE]' syntax, NOTE: All event is not supported by GraphQL API.
 # Required: batchSize field - Default: 0 - Total number of events to batch on server before sending to client.
 # Optional: Market identifier - filter by market
 #           Party identifier - filter by party
 # By default, all events on all markets for all parties will be returned on the stream.
-gq $NODE_URL_GRAPHQL -q 'subscription { busEvents(batchSize: 1, types: [TimeUpdate], marketID: "'$marketID'" ) { type }}'
+# Event types and schema can be found here: https://docs.testnet.vega.xyz/docs/api-howtos/event-stream/
+gq $NODE_URL_GRAPHQL -q 'subscription { busEvents(batchSize: 0, types: [Order], marketID: "'$marketID'" ) { 
+    type 
+    event
+    { 
+        ... on Order {
+              id
+              side
+              price
+              timeInForce
+              side
+              market { id }
+              size
+              remaining
+              party { id }
+              createdAt
+              expiresAt
+              status
+              reference
+              trades { id, size, aggressor }
+              type
+              rejectionReason
+              version
+              updatedAt
+        }
+        ... on TimeUpdate {
+              timestamp
+        }
+    }
+}}'
 # :stream_events__
+
+# Another example, with even deeper graphql nesting would be to select on Governance Proposals:
+#
+# gq $NODE_URL_GRAPHQL -q 'subscription { busEvents(batchSize: 0, types: [Proposal] ) { 
+#     type 
+#     event
+#     { 
+#         ... on Proposal {
+#                 id
+#                 datetime
+#                 yesVotes {
+#                   value
+#                   party { id }
+#                 }
+#                 noVotes {
+#                   value
+#                   party { id }
+#                 }
+#                 party {
+#                   id
+#                 }
+#                 reference
+#                 state
+#                 terms {
+#                   closingDatetime
+#                   enactmentDatetime
+#                   change {
+#                     ... on NewMarket {
+#                       instrument {
+#                         code
+#                       }
+#                       decimalPlaces
+#                       tradingMode {
+#                         ... on ContinuousTrading {
+#                           tickSize
+#                         }
+#                         ... on DiscreteTrading {
+#                           tickSize
+#                           duration
+#                         }
+#                       }
+#                       riskParameters {
+#                         ... on LogNormalRiskModel {
+#                           tau
+#                           riskAversionParameter
+#                           params {
+#                             r
+#                             sigma
+#                             mu
+#                           }
+#                         }
+#                       }
+#                     }
+#                     ... on NewAsset {
+#                       source {
+#                         ... on BuiltinAsset {
+#                           id
+#                         }
+#                       }
+#                     }
+#                   }
+#                }
+#             }
+#         }
+#     }
+# }'
