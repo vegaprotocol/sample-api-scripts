a439565a5435859df029d62f33f7cebac6c2411c
cdm <chris.moores@gmail.com>
2021-04-07 17:36:25 +0100

    Add python submit liquidity order example

---
+++ submit-liquidity-provision/submit-liquidity-order.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python3
+
+"""
+Script language: Python3
+
+Talks to:
+- Vega wallet (REST)
+- Vega node (REST)
+
+Apps/Libraries:
+- REST: requests (https://pypi.org/project/requests/)
+"""
+
+# Note: this file uses smart-tags in comments to section parts of the code to
+# show them as snippets in our documentation. They are not necessary to be
+# included when creating your own custom code.
+#
+# Example of smart-tags:
+#  __something:
+# some code here
+# :something__
+#
+
+import json
+import os
+import requests
+import time
+import helpers
+
+node_url_rest = os.getenv("NODE_URL_REST")
+if not helpers.check_url(node_url_rest):
+    print("Error: Invalid or missing NODE_URL_REST environment variable.")
+    exit(1)
+
+wallet_server_url = os.getenv("WALLETSERVER_URL")
+if not helpers.check_url(wallet_server_url):
+    print("Error: Invalid or missing WALLETSERVER_URL environment variable.")
+    exit(1)
+
+wallet_name = os.getenv("WALLET_NAME")
+if not helpers.check_var(wallet_name):
+    print("Error: Invalid or missing WALLET_NAME environment variable.")
+    exit(1)
+
+wallet_passphrase = os.getenv("WALLET_PASSPHRASE")
+if not helpers.check_var(wallet_passphrase):
+    print("Error: Invalid or missing WALLET_PASSPHRASE environment variable.")
+    exit(1)
+
+# Help guide users against including api version suffix on url
+wallet_server_url = helpers.check_wallet_url(wallet_server_url)
+
+#####################################################################################
+#                           W A L L E T   S E R V I C E                             #
+#####################################################################################
+
+print(f"Logging into wallet: {wallet_name}")
+
+# __login_wallet:
+# Log in to an existing wallet
+req = {"wallet": wallet_name, "passphrase": wallet_passphrase}
+response = requests.post(f"{wallet_server_url}/api/v1/auth/token", json=req)
+helpers.check_response(response)
+token = response.json()["token"]
+# :login_wallet__
+
+assert token != ""
+print("Logged in to wallet successfully")
+
+# __get_pubkey:
+# List key pairs and select public key to use
+headers = {"Authorization": f"Bearer {token}"}
+response = requests.get(f"{wallet_server_url}/api/v1/keys", headers=headers)
+helpers.check_response(response)
+keys = response.json()["keys"]
+pubkey = keys[0]["pub"]
+# :get_pubkey__
+
+assert pubkey != ""
+print("Selected pubkey for signing")
+
+#####################################################################################
+#                               F I N D   M A R K E T                               #
+#####################################################################################
+
+# __get_market:
+# Request the identifier for the market to place on
+url = f"{node_url_rest}/markets"
+response = requests.get(url)
+helpers.check_response(response)
+marketID = response.json()["markets"][0]["id"]
+# :get_market__
+
+assert marketID != ""
+marketName = response.json()["markets"][0]["tradableInstrument"]["instrument"]["name"]
+print(f"Market found: {marketID} {marketName}")
+
+#####################################################################################
+#                   S U B M I T   L I Q U I D I T Y   O R D E R                     #
+#####################################################################################
+
+# __prepare_liquidity_order:
+# Prepare a liquidity order message
+req = {
+    "submission": {
+        "marketId": marketID,
+        "partyId": pubkey,
+        "commitment_amount": "100",
+        "fee": "0.1",
+        "buys": [
+            {
+                "offset": "-1",
+                "proportion": "2",
+                "reference": "PEGGED_REFERENCE_MID"
+            },
+            {
+                "offset": "-2",
+                "proportion": "2",
+                "reference": "PEGGED_REFERENCE_MID"
+            }
+        ],
+        "sells": [
+            {
+                "offset": "1",
+                "proportion": "2",
+                "reference": "PEGGED_REFERENCE_MID"
+            },
+            {
+                "offset": "2",
+                "proportion": "2",
+                "reference": "PEGGED_REFERENCE_MID"
+            },
+            {
+                "offset": "3",
+                "proportion": "2",
+                "reference": "PEGGED_REFERENCE_MID"
+            }
+        ]
+    }
+}
+url = f"{node_url_rest}/liquidity-provisions/prepare/submit"
+response = requests.post(url, json=req)
+helpers.check_response(response)
+prepared_order = response.json()
+# :prepare_liquidity_order__
+
+print(f"Prepared liquidity order for market: {marketID} {marketName}")
+
+# __sign_tx_liquidity_order:
+# Sign the prepared liquidity order transaction
+# Note: Setting propagate to true will also submit to a Vega node
+blob = prepared_order["blob"]
+req = {"tx": blob, "pubKey": pubkey, "propagate": True}
+url = f"{wallet_server_url}/api/v1/messages/sync"
+response = requests.post(url, headers=headers, json=req)
+helpers.check_response(response)
+
+# :sign_tx_liquidity_order__
+
+print("Signed liquidity order and sent to Vega")
+
+# Completed.
