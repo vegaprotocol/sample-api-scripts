7898f59be7699b103f964f297d4bcc8fc0b6e45c
Witold <witold@vega.xyz>
2021-04-08 19:18:59 +0200

    Add amendment and cancellation

--- submit-liquidity-provision/submit-liquidity-provision-order.py
+++ submit-liquidity-provision/submit-liquidity-provision-order.py
@@ -96,23 +96,23 @@ marketName = response.json()["markets"][0]["tradableInstrument"]["instrument"]["
 print(f"Market found: {marketID} {marketName}")
 
 #####################################################################################
-#                   S U B M I T   L I Q U I D I T Y   O R D E R S                   #
+#              S U B M I T   L I Q U I D I T Y   C O M M I T M E N T                #
 #####################################################################################
 
 # Note: commitmentAmount is an integer. For example 123456 is a price of 1.23456,
 # for a market which is configured to have a precision of 5 decimal places.
 
 # __prepare_liquidity_order:
-# Prepare a liquidity commitment order message
+# Prepare a liquidity commitment transaction message
 req = {
     "submission": {
         "marketId": marketID,
         "commitmentAmount": "100",
-        "fee": "0.1",
+        "fee": "0.01",
         "buys": [
             {
                 "offset": "-1",
-                "proportion": "2",
+                "proportion": "1",
                 "reference": "PEGGED_REFERENCE_MID"
             },
             {
@@ -124,7 +124,7 @@ req = {
         "sells": [
             {
                 "offset": "1",
-                "proportion": "2",
+                "proportion": "1",
                 "reference": "PEGGED_REFERENCE_MID"
             },
             {
@@ -134,7 +134,7 @@ req = {
             },
             {
                 "offset": "3",
-                "proportion": "2",
+                "proportion": "5",
                 "reference": "PEGGED_REFERENCE_MID"
             }
         ]
@@ -146,10 +146,10 @@ helpers.check_response(response)
 prepared_order = response.json()
 # :prepare_liquidity_order__
 
-print(f"Prepared liquidity order for market: {marketID} {marketName}")
+print(f"Prepared liquidity commitment for market: {marketID} {marketName}")
 
 # __sign_tx_liquidity_order:
-# Sign the prepared liquidity order transaction
+# Sign the prepared liquidity commitment transaction
 # Note: Setting propagate to true will also submit to a Vega node
 blob = prepared_order["blob"]
 req = {"tx": blob, "pubKey": pubkey, "propagate": True}
@@ -158,6 +158,97 @@ response = requests.post(url, headers=headers, json=req)
 helpers.check_response(response)
 # :sign_tx_liquidity_order__
 
-print("Signed liquidity order and sent to Vega")
+print("Signed liquidity commitment and sent to Vega")
+
+# Completed.
+
+time.sleep(10)
+
+#####################################################################################
+#               A M E N D    L I Q U I D I T Y   C O M M I T M E N T                #
+#####################################################################################
+
+# __prepare_liquidity_order:
+# Prepare a liquidity commitment order message, modify fields to be amended
+
+req = {
+    "submission": {
+        "marketId": marketID,
+        "commitmentAmount": "500",
+        "fee": "0.005",
+        "buys": [
+            {
+                "offset": "-1",
+                "proportion": "1",
+                "reference": "PEGGED_REFERENCE_MID"
+            }
+        ],
+        "sells": [
+            {
+                "offset": "1",
+                "proportion": "1",
+                "reference": "PEGGED_REFERENCE_MID"
+            }
+        ]
+    }
+}
+url = f"{node_url_rest}/liquidity-provisions/prepare/submit"
+response = requests.post(url, json=req)
+helpers.check_response(response)
+prepared_order = response.json()
+# :prepare_liquidity_order__
+
+print(f"Prepared liquidity commitment (amendment) for market: {marketID} {marketName}")
+
+# __sign_tx_liquidity_order:
+# Sign the prepared liquidity commitment transaction
+# Note: Setting propagate to true will also submit to a Vega node
+blob = prepared_order["blob"]
+req = {"tx": blob, "pubKey": pubkey, "propagate": True}
+url = f"{wallet_server_url}/api/v1/messages/sync"
+response = requests.post(url, headers=headers, json=req)
+helpers.check_response(response)
+# :sign_tx_liquidity_order__
+
+print("Signed liquidity commitment (amendment) and sent to Vega")
+
+# Completed.
+
+time.sleep(10)
+
+#####################################################################################
+#               C A N C E L    L I Q U I D I T Y   C O M M I T M E N T              #
+#####################################################################################
+
+# __prepare_liquidity_order:
+# Prepare a liquidity commitment order message, set commitmentAmount to 0, \
+# note that transaction may get rejected if removing previously supplied liquidity 
+# will result in insufficient liquidity for the market
+
+req = {
+    "submission": {
+        "marketId": marketID,
+        "commitmentAmount": "0"
+    }
+}
+url = f"{node_url_rest}/liquidity-provisions/prepare/submit"
+response = requests.post(url, json=req)
+helpers.check_response(response)
+prepared_order = response.json()
+# :prepare_liquidity_order__
+
+print(f"Prepared liquidity commitment (cancellation) for market: {marketID} {marketName}")
+
+# __sign_tx_liquidity_order:
+# Sign the prepared liquidity commitment transaction
+# Note: Setting propagate to true will also submit to a Vega node
+blob = prepared_order["blob"]
+req = {"tx": blob, "pubKey": pubkey, "propagate": True}
+url = f"{wallet_server_url}/api/v1/messages/sync"
+response = requests.post(url, headers=headers, json=req)
+helpers.check_response(response)
+# :sign_tx_liquidity_order__
+
+print("Signed liquidity commitment (cancellation) and sent to Vega")
 
 # Completed.
