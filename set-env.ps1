648533bfe0d329ab4f3c1d92a8cebcc5abd8df09
cdm <chris.moores@gmail.com>
2022-11-18 16:02:58 +0000

    Clean sample-api-scripts file structure for v2 data node support

--- set-env.ps1
+++
@@ -1,9 +0,0 @@
-# Set env variable on a Window computer
-$Env:WALLET_NAME="example_wallet_name"
-$Env:WALLET_PASSPHRASE="example_wallet_passphrase"
-
-# Do not edit below this line:
-$Env:WALLETSERVER_URL="https://wallet.testnet.vega.xyz"
-$Env:NODE_URL_REST="https://lb.testnet.vega.xyz"
-$Env:NODE_URL_GRAPHQL="https://lb.testnet.vega.xyz/query"
-$Env:NODE_URL_GRPC="n06.testnet.vega.xyz:3002"
