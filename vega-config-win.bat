61ceb11d5714f7cb28e6637742cd7c42ff2e19ff
itchydinojake <jake.galvin@btinternet.com>
2022-12-13 11:00:29 +0000

    changed the windows setup to be a single script

---
+++ vega-config-win.bat
@@ -0,0 +1,15 @@
+
+REM Default server configuration used by REST/GRPC/GRAPHQL examples on WINDOWS
+
+REM See other vega-config-XXXX scripts for other examples of network configurations or
+REM update the URLs below to your desired network and wallet setup:
+
+SET DATA_NODE_URL_REST=https://api.n06.testnet.vega.xyz/api/v2
+SET DATA_NODE_URL_GRAPHQL=https://api.n07.testnet.vega.xyz/graphql
+SET DATA_NODE_URL_GRPC=n07.testnet.vega.xyz:3007
+SET WALLET_SERVER_URL=https://wallet.testnet.vega.xyz
+SET MARKET_ID=
+SET PARTY_ID=
+SET ASSET_ID=
+
+echo vega-config has been configured
