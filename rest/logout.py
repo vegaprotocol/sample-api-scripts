fb5f966373e63ff4d880ab2054a4a1e05a625011
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2023-03-28 16:04:12 +0100

    Change logout script

--- rest/logout.py
+++ rest/logout.py
@@ -1,21 +1,14 @@
-import os
-import helpers
+import json
+import requests
+from login import token
 
-wallet_server_url = helpers.get_from_env("WALLET_SERVER_URL")
-
-
-def perform_logout():
-    if os.path.exists("token.temp"):
-        print(f"Using API: {wallet_server_url}")
-        os.remove("token.temp")
-        # Clean up any env vars related to wallet user
-        os.unsetenv("WALLET_NAME")
-        os.unsetenv("WALLET_PASSPHRASE")
-        print("Log out complete (removed token file and environment vars)")
-    else:
-        print("The token file does not exist, try `python3 login.py`")
-
-
-if __name__ == "__main__":
-    perform_logout()
-    exit(0)
+url = "http://localhost:1789/api/v2/requests"
+connect_payload = "{\n        \"id\": \"1\",\n        \"jsonrpc\": \"2.0\",\n        \"method\": \"client.disconnect_wallet\"\n    }"
+connect_headers = {
+  'Content-Type': 'application/json-rpc',
+  'Accept': 'application/json-rpc', 
+  'Origin': 'application/json-rpc', 
+  'Authorization': token
+}
+connectionResponse = requests.request("POST", url, headers=connect_headers, data=connect_payload)
+print(connectionResponse.text)
