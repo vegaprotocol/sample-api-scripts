2658a321d472c857bb773779e963b16595f1bfbc
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2023-05-04 13:50:39 +0100

    pegged order scripts

--- rest/pegged-order-submit.py
+++ rest/pegged-order-submit.py
@@ -74,11 +74,31 @@ print()
 
 # __sign_tx_pegged_order:
 # Sign the transaction with a pegged order submission command
-# Hint: Setting propagate to true will also submit to a Vega node
-url = f"{wallet_server_url}/api/v2/requests"
-headers = {"Authorization": f"Bearer {token}"}
-response = requests.post(url, headers=headers, json=submission)
-helpers.check_response(response)
+url = "http://localhost:1789/api/v2/requests"
+
+payload1 = {
+    "id": "1",
+    "jsonrpc": "2.0",
+    "method": "client.send_transaction",
+    "params": {
+        "publicKey": pubkey,
+        "sendingMode": "TYPE_SYNC",
+        "transaction": submission
+    }
+}
+
+payload = json.dumps(payload1)
+
+headers = {
+  'Content-Type': 'application/json-rpc',
+  'Accept': 'application/json-rpc',
+  'Origin': 'application/json-rpc', 
+  'Authorization': f'{token}'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
 # :sign_tx_pegged_order__
 
 print(json.dumps(response.json(), indent=4, sort_keys=True))
