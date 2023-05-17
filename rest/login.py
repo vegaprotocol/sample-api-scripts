e480eb5617dbc0b0be5a58fbe2fac7dc11299c25
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2023-05-17 11:55:29 +0100

    Better login error message

--- rest/login.py
+++ rest/login.py
@@ -1,26 +1,50 @@
-
 import json
 import requests
 
 url = "http://localhost:1789/api/v2/requests"
-connect_payload = "{\n        \"id\": \"1\",\n        \"jsonrpc\": \"2.0\",\n        \"method\": \"client.connect_wallet\"\n    }"
+connect_payload = {
+    "id": "1",
+    "jsonrpc": "2.0",
+    "method": "client.connect_wallet"
+}
 connect_headers = {
-  'Content-Type': 'application/json-rpc',
-  'Accept': 'application/json-rpc', 
-  'Origin': 'application/json-rpc'
+    'Content-Type': 'application/json-rpc',
+    'Accept': 'application/json-rpc',
+    'Origin': 'application/json-rpc'
 }
-connectionResponse = requests.request("POST", url, headers=connect_headers, data=connect_payload)
 
-authorize_payload = "{\n        \"id\": \"1\",\n        \"jsonrpc\": \"2.0\",\n        \"method\": \"client.list_keys\"\n    }"
+try:
+    connectionResponse = requests.post(url, headers=connect_headers, json=connect_payload)
+    connectionResponse.raise_for_status()  # Raise an exception for unsuccessful responses
+except requests.exceptions.RequestException as e:
+    print("Error connecting to the API - Make sure you have a wallet connection open and have imported the right vega-config:", str(e))
+    exit(1)
+
+authorize_payload = {
+    "id": "1",
+    "jsonrpc": "2.0",
+    "method": "client.list_keys"
+}
 authorize_headers = {
-  'Content-Type': 'application/json-rpc',
-  'Accept': 'application/json-rpc',
-  'Origin': 'application/json-rpc',
-  "Authorization": connectionResponse.headers["Authorization"]
+    'Content-Type': 'application/json-rpc',
+    'Accept': 'application/json-rpc',
+    'Origin': 'application/json-rpc',
+    "Authorization": connectionResponse.headers["Authorization"]
 }
 
-authorizationResponse = requests.request("POST", url, headers=authorize_headers, data=authorize_payload)
+try:
+    authorizationResponse = requests.post(url, headers=authorize_headers, json=authorize_payload)
+    authorizationResponse.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print("Error authorizing request:", str(e))
+    exit(1)
 
-token = connectionResponse.headers["Authorization"]
-authConent = json.loads(authorizationResponse.content)
-pubkey = authConent['result']['keys'][0]['publicKey']
+try:
+    token = connectionResponse.headers["Authorization"]
+    authContent = json.loads(authorizationResponse.content)
+    pubkey = authContent['result']['keys'][0]['publicKey']
+except (KeyError, IndexError, json.JSONDecodeError) as e:
+    print("Error parsing authorization response:", str(e))
+    exit(1)
+
+# Rest of your code...
